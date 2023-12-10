077b9e7423e9c6bef4b3dd5cad9979f05b19440a
legionnae <90042717+legionnae@users.noreply.github.com>
2023-12-10 19:30:59 +0800

    Update AchievementsSection.jsx

--- components/AchievementsSection.jsx
+++ components/AchievementsSection.jsx
@@ -16,23 +16,20 @@ const achievementsList = [
 
 ];
 const AchievementsSection = () => {
-    return(
-        <div className=" xl:gap-3 sm:py-3 xl:px-12">
-            <div className="border-[#33353F] border rounded-md py-8 px-16 flex flex-row items-center justify-between">
-            {achievementsList.map((achievement) => {
-                return(
-                    <div 
-                        className="flex flex-col items-center justify-center mx-4">
-                        <h2 className="text-white text-4xl font-bold">
-                            {achievement.value}
-                        </h2>
-                        <p className="text-[#ADB7BE] text-base">{achievement.metric}</p>
-                    </div>
-                )
-            }
-            )}
-            </div>
-            </div>
-    )
-}
-export default AchievementsSection;
+    return (
+      <div className="xl:gap-3 sm:py-3 xl:px-12">
+        <div className="border-[#33353F] border rounded-md py-8 px-16 flex flex-row items-center justify-between">
+          {achievementsList.map((achievement) => {
+            return (
+              <div key={achievement.metric} className="flex flex-col items-center justify-center mx-4">
+                <h2 className="text-white text-4xl font-bold">{achievement.value}</h2>
+                <p className="text-[#ADB7BE] text-base">{achievement.metric}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  };
+  
+  export default AchievementsSection;
